fce2210b3bbf00699841b58e95ca617f5cfbe8b8
Henilhra <131961135+Henilhra@users.noreply.github.com>
2026-02-11 04:21:06 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,15 @@
 export default {
   async fetch(request) {
-    const notion = "https://broad-windshield-f43.notion.site/Aeroware-tech-51d4a7a25583490aa4ac3a1389cc255e";
+    const notion = "https://broad-windshield-f43.notion.site";
     const url = new URL(request.url);
 
-    const response = await fetch(notion + url.pathname, {
-      headers: request.headers
+    const target = notion + "/Aeroware-tech-51d4a7a25583490aa4ac3a1389cc255e" + url.pathname + url.search;
+
+    const response = await fetch(target, {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "*/*"
+      }
     });
 
     return new Response(response.body, response);
